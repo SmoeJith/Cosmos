298491a15f7bb4bdb1933c093089bc1c2fec66b6
SmoeJith <31860196+SmoeJith@users.noreply.github.com>
2023-05-31 00:37:36 -0400

    fix(namespace): fixed issues related to a commit in the main branch

--- cosmos/settings.py
+++ cosmos/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Cosmos.urls'
+ROOT_URLCONF = 'cosmos.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Cosmos.wsgi.application'
+WSGI_APPLICATION = 'cosmos.wsgi.application'
 
 
 # Database
